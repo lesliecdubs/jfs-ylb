3f564d13feb8d0d1c2f1751b63e84414fb356b99
Devon Winders <dwinders@canvas.is>
2016-08-02 18:58:56 -0600

    update webpack location

--- config.rb
+++ config.rb
@@ -71,7 +71,7 @@ end
 activate :external_pipeline,
          name: :webpack,
          command: build? ?
-         './node_modules/webpack/bin/webpack.js --bail' :
-         './node_modules/webpack/bin/webpack.js --watch -d',
+         'webpack --bail' :
+         'webpack --watch -d',
          source: ".tmp/dist",
          latency: 1
